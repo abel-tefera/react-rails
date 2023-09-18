08828f2ef31fba985d26e506367cb05ad63b39f1
abel-tefera <abeltefera16@gmail.com>
2023-09-18 12:50:40 +0300

    Set up db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create(message: "Hello, World!")
+Greeting.create(message: "Hello, Abel!")
+Greeting.create(message: "Hello, Haymi!")
+Greeting.create(message: "Hello, David!")
+Greeting.create(message: "Hello, Racheal!")
